315d0e46e79b0e19883255b239fb3f6b5e6082cf
Martijn van Ginkel <martijnvanginkel2@gmail.com>
2021-02-28 20:48:44 +0100

    feat: created seperate pool connection for table creation cause of es6 import issues

--- server/src/db/tables.ts
+++ server/src/db/tables.ts
@@ -1,16 +1,39 @@
-import { pool } from './connection'
+// import { pool } from './connection'
+// const pool = require('./pool')
+// import { Pool } from 'pg'
+const pg = require('pg')
+
+const pool = new pg.Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    database: process.env.DB_NAME,
+    max: 5,
+    connectionTimeoutMillis: 0,
+    idleTimeoutMillis: 0,
+})
+
+// const pool = require('./connection')
 
 const createGamesTable = async () => {
     const tableName = 'games'
     const query = `
         id SERIAL PRIMARY KEY,
-        game_id uuid DEFAULT uuid_generate_v4 (), 
+        game_id uuid DEFAULT uuid_generate_v4 (),
     `
 
     try {
-        await pool.query(`DROP TABLE IF EXISTS ${tableName} CASCADE`)
-        await pool.query(`CREATE TABLE ${tableName} (${query});`)
+        // await pool.query(`DROP TABLE IF EXISTS ${'asdf'} CASCADE`)
+        await pool.query(`CREATE TABLE roles(
+            role_id serial PRIMARY KEY,
+            role_name VARCHAR (255) UNIQUE NOT NULL
+         );`)
+        //  console.log()
     } catch (error) {
         console.log(`${error}`)
     }
 }
+// console.log('asdf')
+createGamesTable()
+// return
