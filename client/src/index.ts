bd0fa8ac25309d1f56d4f629eefcea95af83fd87
Martijn van Ginkel <martijnvanginkel2@gmail.com>
2021-04-08 20:56:08 +0200

    refactor: cleaned up index

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,16 +1,9 @@
-// import { Images } from './game/images/Images'
-// import { Map } from './game/map/Map'
-// import { MessageDistributor } from './game/managers/MessageDistributor'
 import { defineComponents } from './views/components/defineComponents'
 import { createRouteManager } from './views/RouteManager'
 import { io } from 'socket.io-client'
 import { createNewGame, GameInitInfo } from './game/Game'
-// import { createNewGame, GameInfo } from './game/Game'
 
 defineComponents()
-// export const images = new Images()
-// export const map = new Map()
-// export const messageDistributor = new MessageDistributor()
 
 const routeManager = createRouteManager()
 routeManager.goToRoute('home')
